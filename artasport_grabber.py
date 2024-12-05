0609f2d0f6979bb6a0e663e681fb72f4a19a30ab
frontov <roman@frontov.ru>
2024-12-05 23:21:42 +0300

    fix artasport

--- artasport_grabber.py
+++ artasport_grabber.py
@@ -1,6 +1,8 @@
 import requests
 from lxml import html
 
+from date_utils import russian_date_to_timestamp
+
 
 class Event:
     def __init__(self, name, link, description, kind, sub_kind, date_start, region):
@@ -13,7 +15,20 @@ class Event:
         self.region = region
 
 
-def parse_events(url):
+def cut_string_by_second_space(s):
+    # Split the string by spaces
+    parts = s.split(' ')
+
+    # Check if there are at least two spaces
+    if len(parts) < 3:
+        return s  # Return the original string if there are less than two spaces
+
+    # Join the first two parts with a space and the rest of the parts
+    return ' '.join(parts[:2])
+
+
+def parse_events():
+    url = 'https://arta-sport.ru/predstoyaschie-meropriyatiya/'
     response = requests.get(url)
     tree = html.fromstring(response.content)
 
@@ -22,25 +37,31 @@ def parse_events(url):
         name = event.xpath('.//*[@class="name"]/text()')[0]
         link = event.xpath('.//a/@href')[0]
         kind = event.xpath('.//span[@class="kind"]/text()')
-        sub_kind = event.xpath('.//span[@class="sub-kind"]/text()')[0]
-        date_start = event.xpath('.//span[@class="date-start"]/text()')[0]
-        region = event.xpath('.//span[@class="region"]/text()')[0]
+        # sub_kind = event.xpath('.//span[@class="sub-kind"]/text()')[0]
+        date_start = event.xpath('.//div[@class="data"]/text()')[0]
+        print(date_start)
+        date_start=cut_string_by_second_space(date_start)
+        print(date_start)
 
-        event_obj = Event(name, link, '', kind, sub_kind, date_start, region)
+        date_start = russian_date_to_timestamp(date_start)
+        print(date_start)
+        region = event.xpath('.//div[@class="adr"]/text()')[0]
+
+        event_obj = Event(name, link, '', kind, '', date_start[0], region)
         events.append(event_obj)
 
     return events
 
 
 url = 'https://arta-sport.ru/predstoyaschie-meropriyatiya/'
-events = parse_events(url)
+# events = parse_events(url)
 
-for event in events:
-    print(f"Name: {event.name}")
-    print(f"Link: {event.link}")
-    print(f"Description: {event.description}")
-    print(f"Kind: {', '.join(event.kind)}")
-    print(f"Sub-kind: {event.sub_kind}")
-    print(f"Date Start: {event.date_start}")
-    print(f"Region: {event.region}")
-    print()
+# for event in events:
+#     print(f"Name: {event.name}")
+#     print(f"Link: {event.link}")
+#     print(f"Description: {event.description}")
+#     print(f"Kind: {', '.join(event.kind)}")
+#     print(f"Sub-kind: {event.sub_kind}")
+#     print(f"Date Start: {event.date_start}")
+#     print(f"Region: {event.region}")
+#     print()
