b1ff9c3e851f260bc5853e1b299f5945beb0219d
frontov <roman@frontov.ru>
2025-02-01 15:14:38 +0300

    cry uvicorn.run

--- uvicornconf.py
+++ uvicornconf.py
@@ -8,6 +8,8 @@ ssl_context.load_cert_chain('/etc/letsencrypt/live/back.where2race.ru/fullchain.
                             '/etc/letsencrypt/live/back.where2race.ru/privkey.pem')
 
 if __name__ == "__main__":
+    print(os.getcwd())
+    print(os.listdir(os.getcwd()))
     import uvicorn
-    uvicorn.run("api:app", host="127.0.0.1", port=8000)
+    uvicorn.run("api:app", host="127.0.0.1", port=8000, ssl = ssl_context)
     # uvicorn.run("api:app", host="0.0.0.0", port=8000)
